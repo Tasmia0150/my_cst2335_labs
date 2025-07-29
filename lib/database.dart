cc34b0b41ae85bd3c9699c610a5d2df50d776e6d
Tasmia0150 <abonitasmiah@gmail.com>
2025-07-29 12:43:36 -0400

    Lab 8 completed and fixed.

--- lib/database.dart
+++ lib/database.dart
@@ -4,6 +4,7 @@ import 'entity/shopping_item.dart';
 import 'dao/shopping_dao.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
 
+
 part 'database.g.dart';
 
 @Database(version: 1, entities: [ShoppingItem])
